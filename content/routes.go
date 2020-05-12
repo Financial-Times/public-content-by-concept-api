d6e44ecf7a00d533bf515be9e0fbe5c7a5248faa
Ivan Nikolov <ivan.nikolov@ft.com>
2020-05-12 17:10:06 +0300

    Separate the healtchecks from the handlers

--- content/routes.go
+++ content/routes.go
@@ -10,7 +10,6 @@ import (
 	"time"
 
 	"github.com/Financial-Times/api-endpoint"
-	fthealth "github.com/Financial-Times/go-fthealth/v1_1"
 	"github.com/Financial-Times/http-handlers-go/httphandlers"
 	st "github.com/Financial-Times/service-status-go/httphandlers"
 	"github.com/rcrowley/go-metrics"
@@ -23,31 +22,61 @@ import (
 
 const uuidRegex = "([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})$"
 
-func StartServer(addr string, heathConfig HealthConfig, neoURL string, neoConf neoutils.ConnectionConfig, cacheTime time.Duration) (func(), error) {
+type ServerConfig struct {
+	Port           string
+	AppSystemCode  string
+	AppName        string
+	AppDescription string
+	NeoURL         string
+	APIYMLPath     string
+	CacheTime      time.Duration
+	NeoConfig      neoutils.ConnectionConfig
+}
 
-	cbcService, err := NewContentByConceptService(neoURL, neoConf)
+func StartServer(config ServerConfig) (func(), error) {
+
+	apiEndpoint, err := api.NewAPIEndpointForFile(config.APIYMLPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to serve the API Endpoint for this service from file %s: %w", config.APIYMLPath, err)
+	}
+	cbcService, err := NewContentByConceptService(config.NeoURL, config.NeoConfig)
 	if err != nil {
 		return nil, fmt.Errorf("could not create concept service: %w", err)
 	}
 
 	handler := ContentByConceptHandler{
 		ContentService:     cbcService,
-		CacheControlHeader: strconv.FormatFloat(cacheTime.Seconds(), 'f', 0, 64),
+		CacheControlHeader: strconv.FormatFloat(config.CacheTime.Seconds(), 'f', 0, 64),
 		UUIDMatcher:        regexp.MustCompile(uuidRegex),
+	}
+
+	hs := &HealthcheckService{
+		AppSystemCode:  config.AppSystemCode,
+		AppName:        config.AppName,
+		AppDescription: config.AppDescription,
+		ConnChecker:    cbcService.CheckConnection,
 	}
 
 	router := mux.NewRouter()
 	logger.Info("registering handlers")
 	router.HandleFunc("/content", handler.GetContentByConcept).Methods(http.MethodGet)
 
-	monitoringRouter := handler.RegisterAdminHandlers(router, heathConfig)
+	logger.Info("Registering healthcheck handlers")
+	router.HandleFunc("/__health", hs.HealthHandler()).Methods(http.MethodGet)
+	router.HandleFunc(st.BuildInfoPath, st.BuildInfoHandler).Methods(http.MethodGet)
+	router.HandleFunc(api.DefaultPath, apiEndpoint.ServeHTTP).Methods(http.MethodGet)
+	router.HandleFunc(st.GTGPath, st.NewGoodToGoHandler(hs.GTG)).Methods(http.MethodGet)
+
+	var monitoringRouter http.Handler = router
+	monitoringRouter = httphandlers.TransactionAwareRequestLoggingHandler(log.StandardLogger(), monitoringRouter)
+	monitoringRouter = httphandlers.HTTPMetricsHandler(metrics.DefaultRegistry, monitoringRouter)
 
 	srv := http.Server{
-		Addr:    addr,
+		Addr:    ":" + config.Port,
 		Handler: monitoringRouter,
 	}
 
-	logger.Infof("Application started on address %s with args %s", addr, os.Args)
+	logger.Infof("Application started on port %s with args %s", config.Port, os.Args)
 	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 		logger.WithError(err).Error("server closed with unexpected error")
 	}
@@ -60,30 +89,4 @@ func StartServer(addr string, heathConfig HealthConfig, neoURL string, neoConf n
 			logger.WithError(err).Error("server shutdown with unexpected error")
 		}
 	}, nil
-}
-
-func (h *ContentByConceptHandler) RegisterAdminHandlers(router *mux.Router, appConf HealthConfig) http.Handler {
-	logger.Info("Registering healthcheck handlers")
-
-	hc := fthealth.TimedHealthCheck{
-		HealthCheck: fthealth.HealthCheck{
-			SystemCode:  appConf.AppSystemCode,
-			Name:        appConf.AppName,
-			Description: appConf.AppDescription,
-			Checks:      h.checks(),
-		},
-		Timeout: 10 * time.Second,
-	}
-
-	router.HandleFunc("/__health", fthealth.Handler(hc))
-	router.HandleFunc(st.BuildInfoPath, st.BuildInfoHandler)
-	router.HandleFunc(api.DefaultPath, appConf.ApiEndpoint.ServeHTTP)
-	router.HandleFunc(st.GTGPath, st.NewGoodToGoHandler(h.gtg))
-
-	var monitoringRouter http.Handler = router
-	monitoringRouter = httphandlers.TransactionAwareRequestLoggingHandler(log.StandardLogger(), monitoringRouter)
-	if appConf.RequestLoggingEnabled {
-		monitoringRouter = httphandlers.HTTPMetricsHandler(metrics.DefaultRegistry, monitoringRouter)
-	}
-	return monitoringRouter
 }
