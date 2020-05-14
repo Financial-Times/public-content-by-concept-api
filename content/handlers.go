3ab375644b703cef31d4000649ebe5fe924d8b0d
Ivan Nikolov <ivan.nikolov@ft.com>
2020-05-14 15:36:51 +0300

    Refactor handler code

--- content/handlers.go
+++ content/handlers.go
@@ -9,8 +9,6 @@ import (
 	"strings"
 	"time"
 
-	"regexp"
-
 	"github.com/Financial-Times/go-logger"
 	transactionidutils "github.com/Financial-Times/transactionid-utils-go"
 )
@@ -18,47 +16,39 @@ import (
 const (
 	defaultPage    = 1
 	thingURIPrefix = "http://api.ft.com/things/"
+	dateTimeLayout = "2006-01-02"
 )
 
 type dbService interface {
-	CheckConnection() (string, error)
 	GetContentForConcept(conceptUUID string, params RequestParams) (contentList, bool, error)
 }
-type ContentByConceptHandler struct {
+
+type Handler struct {
 	ContentService     dbService
 	CacheControlHeader string
-	UUIDMatcher        *regexp.Regexp
 }
 
-func (ch *ContentByConceptHandler) GetContentByConcept(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) GetContentByConcept(w http.ResponseWriter, r *http.Request) {
 	transID := transactionidutils.GetTransactionIDFromRequest(r)
+
 	m, err := url.ParseQuery(r.URL.RawQuery)
 	if err != nil {
 		logger.WithError(err).WithTransactionID(transID).Error("Could not parse request url")
 		return
 	}
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.Header().Set("X-Request-Id", transID)
+	w.Header().Set(transactionidutils.TransactionIDHeader, transID)
 	logger.WithTransactionID(transID).Infof("request url is %s", r.URL.RawQuery)
 
-	_, isAnnotatedByPresent := m["isAnnotatedBy"]
-	if !isAnnotatedByPresent {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"message": "Missing or empty query parameter isAnnotatedBy. Expecting valid absolute concept URI."}`))
-		return
-	}
-
-	conceptURI := m["isAnnotatedBy"][0]
+	conceptURI := m.Get("isAnnotatedBy")
 	if conceptURI == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"message": "Missing concept URI."}`))
+		writeJSONMessage(w, http.StatusBadRequest, "Missing or empty query parameter isAnnotatedBy. Expecting valid absolute concept URI.")
 		return
 	}
 
 	conceptUUID := strings.TrimPrefix(conceptURI, thingURIPrefix)
-	if !ch.UUIDMatcher.MatchString(conceptUUID) {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"message": "ID extracted from request URL was not valid uuid"}`))
+	if !UUIDRegex.MatchString(conceptUUID) {
+		writeJSONMessage(w, http.StatusBadRequest, fmt.Sprintf("%s extracted from request URL was not valid uuid", conceptUUID))
 		return
 	}
 
@@ -69,16 +59,14 @@ func (ch *ContentByConceptHandler) GetContentByConcept(w http.ResponseWriter, r 
 		if err != nil {
 			msg := fmt.Sprintf("provided value for page, %s, could not be parsed.", pageParam)
 			logger.WithError(err).WithTransactionID(transID).WithUUID(conceptUUID).Error(msg)
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(`{"message": ` + msg + `}`))
+			writeJSONMessage(w, http.StatusBadRequest, msg)
 			return
 		}
 
 		if page < defaultPage {
 			msg := fmt.Sprintf("provided value for page should be greater than: %v", defaultPage)
 			logger.WithTransactionID(transID).WithUUID(conceptUUID).Debugf(msg)
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(`{"message": ` + msg + `}`))
+			writeJSONMessage(w, http.StatusBadRequest, msg)
 			return
 		}
 	}
@@ -87,7 +75,7 @@ func (ch *ContentByConceptHandler) GetContentByConcept(w http.ResponseWriter, r 
 	var contentLimit int
 
 	if limitParam == "" {
-		logger.WithTransactionID(transID).WithUUID(conceptUUID).Debugf("No contentLimit provided. Using default: %v", defaultLimit)
+		logger.WithTransactionID(transID).WithUUID(conceptUUID).Debugf("No contentLimit provided. Using default: %d", defaultLimit)
 		contentLimit = defaultLimit
 	} else {
 		contentLimit, err = strconv.Atoi(limitParam)
@@ -104,12 +92,11 @@ func (ch *ContentByConceptHandler) GetContentByConcept(w http.ResponseWriter, r 
 	if fromDateParam == "" {
 		logger.WithTransactionID(transID).WithUUID(conceptUUID).Debug("no fromDate url param supplied")
 	} else {
-		fromDateTime, err := time.Parse("2006-01-02", fromDateParam)
+		fromDateTime, err := time.Parse(dateTimeLayout, fromDateParam)
 		if err != nil {
 			msg := fmt.Sprintf("From date value %s could not be parsed", fromDateParam)
 			logger.WithError(err).WithTransactionID(transID).WithUUID(conceptUUID).Error(msg)
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(`{"message": ` + msg + `}`))
+			writeJSONMessage(w, http.StatusBadRequest, msg)
 			return
 		}
 		fromDateEpoch = fromDateTime.Unix()
@@ -118,12 +105,11 @@ func (ch *ContentByConceptHandler) GetContentByConcept(w http.ResponseWriter, r 
 	if toDateParam == "" {
 		logger.WithTransactionID(transID).WithUUID(conceptUUID).Debug("no toDate url param supplied")
 	} else {
-		toDateTime, err := time.Parse("2006-01-02", toDateParam)
+		toDateTime, err := time.Parse(dateTimeLayout, toDateParam)
 		if err != nil {
 			msg := fmt.Sprintf("To date value %s could not be parsed", toDateParam)
 			logger.WithError(err).WithTransactionID(transID).WithUUID(conceptUUID).Error(msg)
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(`{"message": ` + msg + `}`))
+			writeJSONMessage(w, http.StatusBadRequest, msg)
 			return
 		}
 		toDateEpoch = toDateTime.Unix()
@@ -136,30 +122,32 @@ func (ch *ContentByConceptHandler) GetContentByConcept(w http.ResponseWriter, r 
 		toDateEpoch:   toDateEpoch,
 	}
 
-	contentList, found, err := ch.ContentService.GetContentForConcept(conceptUUID, requestParams)
+	contentList, found, err := h.ContentService.GetContentForConcept(conceptUUID, requestParams)
 	if err != nil {
 		msg := fmt.Sprintf("Backend error returning content for concept with uuid %s", conceptUUID)
 		logger.WithError(err).WithTransactionID(transID).WithUUID(conceptUUID).Error(msg)
-		w.WriteHeader(http.StatusServiceUnavailable)
-		w.Write([]byte(`{"message": ` + msg + `}`))
+		writeJSONMessage(w, http.StatusServiceUnavailable, msg)
 		return
 	}
 	if !found {
 		msg := fmt.Sprintf("No content found for concept with uuid %s", conceptUUID)
 		logger.WithTransactionID(transID).WithUUID(conceptUUID).Info(msg)
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(`{"message": ` + msg + `}`))
+		writeJSONMessage(w, http.StatusNotFound, msg)
 		return
 	}
 
-	w.Header().Set("Cache-Control", ch.CacheControlHeader)
+	w.Header().Set("Cache-Control", h.CacheControlHeader)
 	w.WriteHeader(http.StatusOK)
 
 	if err = json.NewEncoder(w).Encode(contentList); err != nil {
 		msg := fmt.Sprintf("Error parsing returned content list for concept with uuid %s", conceptUUID)
 		logger.WithError(err).WithTransactionID(transID).WithUUID(conceptUUID).Error(msg)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(`{"message": ` + msg + `}`))
+		writeJSONMessage(w, http.StatusInternalServerError, msg)
 		return
 	}
+}
+
+func writeJSONMessage(w http.ResponseWriter, status int, msg string) {
+	w.WriteHeader(status)
+	_, _ = w.Write([]byte(`{"message": "` + msg + `"}`))
 }
