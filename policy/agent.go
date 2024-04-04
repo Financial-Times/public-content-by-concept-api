6cddf0f80feca0189af36faaaf472e63253ff72a
Anna Grancharova <anna.grancharova@ft.com>
2024-04-04 17:51:41 +0300

    Added: logs with information on forbidden requests

--- policy/agent.go
+++ policy/agent.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/Financial-Times/go-logger/v2"
+	transactionidutils "github.com/Financial-Times/transactionid-utils-go"
 )
 
 const (
@@ -17,17 +18,23 @@ type Result struct {
 	IsAuthorizedForPublication bool     `json:"is_authorized_for_publication"`
 	AddFilterByPublication     bool     `json:"add_filter_by_publication"`
 	Publications               []string `json:"xpolicy_publications"`
+	Reasons                    []string `json:"reasons"`
 }
 
 func IsAuthorizedPublication(n http.Handler, w http.ResponseWriter, req *http.Request, log *logger.UPPLogger, r Result) {
+	transID := transactionidutils.GetTransactionIDFromRequest(req)
+	logEntry := log.WithTransactionID(transID)
+
 	if r.IsAuthorizedForPublication {
 		n.ServeHTTP(w, req)
 	} else {
+
 		if r.AddFilterByPublication {
-			log.Infof("Adding filter for publications: %s", r.Publications)
+			logEntry.Infof("Adding filter for publications: %s", r.Publications)
 			req.URL.RawQuery = req.URL.RawQuery + fmt.Sprintf("&publication=%s", strings.Join(r.Publications, ","))
 			n.ServeHTTP(w, req)
 		} else {
+			logEntry.Infof("Request is forbidden due to missing or non-matching access policies: %s", r.Reasons)
 			http.Error(w, "Forbidden", http.StatusForbidden)
 		}
 	}
